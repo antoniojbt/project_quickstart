28887a91b9d621739ef35ea292947494cee1988e
AntonioJBT <a.berlanga@imperial.ac.uk>
2017-01-23 14:36:43 +0000

    updates to setup files

--- templates/python_packaging.dir/setup.py
+++ templates/python_packaging.dir/setup.py
@@ -2,6 +2,8 @@
 # For a fuller example see: https://github.com/CGATOxford/UMI-tools/blob/master/setup.py
 # Or: https://github.com/CGATOxford/cgat/blob/master/setup.py
 
+# See also this example: https://github.com/pypa/sampleproject/blob/master/setup.py
+
 # TO DO: update with further options such as include README.rst and others when ready
 
 # TO DO: to add tests see https://python-packaging.readthedocs.io/en/latest/testing.html
