7293a995632aef8123fae736b45f8ff5672a22d5
Antonio <AntonioJBT@users.noreply.github.com>
2017-04-07 15:47:25 +0100

    Update __init__.py

--- project_quickstart/__init__.py
+++ project_quickstart/__init__.py
@@ -1,7 +1,9 @@
 import os
 
 _ROOT = os.path.abspath(os.path.dirname(__file__))
-def get_data(path):
+def getDir(path):
+    ''' Search path for the 'xxxx' directory for copying within cwd '''
     return os.path.join(_ROOT, 'data', path)
+    return os.path.join(_ROOT, f'project_quickstart-{prog_version}', path)
 
-print get_data('resource1/foo.txt')
+#print(getDir('templates'))
