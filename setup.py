b7126a2c326d93b5ad1bf07e0909bff247c27d64
AntonioJBT <a.berlanga@imperial.ac.uk>
2017-04-16 22:43:47 +0100

    ....

--- setup.py
+++ setup.py
@@ -42,42 +42,7 @@ import os
 here = os.path.abspath(os.path.dirname(__file__))
 print(here)
 
-# Set up calling parameters from INI file:
-# Modules with Py2 to 3 conflicts
-try:
-    import configparser
-except ImportError:  # Py2 to Py3
-    import ConfigParser as configparser
 
-# Global variable for configuration file ('.ini')
-# allow_no_value addition is from:
-# https://github.com/docopt/docopt/blob/master/examples/config_file_example.py
-# By using `allow_no_value=True` we are allowed to
-# write `--force` instead of `--force=true` below.
-CONFIG = configparser.ConfigParser(allow_no_value = True)
-
-CONFIG.read(os.path.join(here, str('project_quickstart' + '.ini')))
-# str(CONFIG['metadata']['project_name'] + '.ini'))) 
-
-# Print keys (sections):
-print('Values for setup.py:', '\n')
-for key in CONFIG:
-    for value in CONFIG[key]:
-        print(key, value, CONFIG[key][value])
-#################
-
-
-#################
-# Get version:
-sys.path.insert(0, CONFIG['metadata']['project_name'])
-import version
-
-version = version.__version__
-print(version)
-#################
-
-
-#################
 # Get info from README and version.py:
 # Get Ptyhon modules required:
 install_requires = []
@@ -93,79 +58,31 @@ print(install_requires)
 # Use README as long description if desired, otherwise get it from INI file (or
 # write it out in setup()):
 
-#with open(os.path.join(here, 'README.rst'), 'rt', encoding='utf-8') as readme:
-#    description = readme.read()
-
-# Give warning:
-class CustomInstall(install):
-    def initialize_options(self):
-        if sys.version < '3.5':
-            print('Error during installation: ', '\n',
-                    CONFIG['metadata']['project_name'],
-                    ' requires Python 3.5 or higher.',
-                    'Exiting...')
-            sys.exit(1)
-
-        return install.initialize_options(self)
-
-entry_points = str('{"console_scripts"'
-                  + ' : '
-                  + ' [ '
-                  + '"'
-                  + CONFIG['metadata']['project_name']
-                  + '.py'
-                  + ' = '
-                  + CONFIG['metadata']['project_name']
-                  + '.'
-                  + CONFIG['metadata']['project_name']
-                  + ' : main'
-                  + '" '
-                  + ']}'
-                  )
-print(entry_points)
-#################
+with open(os.path.join(here, 'README.rst'), 'rt', encoding='utf-8') as readme:
+    description = readme.read()
 
 
-#################
+
 # Actual setup.py instructions
 # Python docs: https://docs.python.org/3.6/distutils/setupscript.html 
 # Tutorial: http://python-packaging.readthedocs.io/en/latest/
 setup(
       # Package metadata:
-      name = CONFIG['metadata']['project_name'],
-      version = CONFIG['metadata']['version'],
-      url = CONFIG['metadata']['project_url'],
-      download_url = CONFIG['metadata']['download_url'],
-      author = CONFIG['metadata']['author_name'],
-      author_email = CONFIG['metadata']['author_email'],
-      license = CONFIG['metadata']['license'],
-#      classifiers = [CONFIG['metadata']['classifiers_setup']], 
-       # needs to be passed as list
-       # gives many errors when registering manually in pip
-      description = CONFIG['metadata']['project_short_description'],
-      keywords = CONFIG['metadata']['keywords'],
-      long_description = CONFIG['metadata']['long_description'], #long_description = description,
+      name = project_quickstart,
+     # version = 0.2.3,
+     # url = CONFIG['metadata']['project_url'],
+     # download_url = CONFIG['metadata']['download_url'],
+     # author = CONFIG['metadata']['author_name'],
+     # author_email = CONFIG['metadata']['author_email'],
+     # license = CONFIG['metadata']['license'],
+      description = 'xxxxx',
+      keywords = 'keywords toott tootr',
+      long_description = description,
       # Package information:
-#      packages = find_packages(CONFIG['metadata']['project_name']),
-      packages = find_packages(),
-      #[CONFIG['metadata']['packages_setup']], # needs to be passed as list
+      packages = ['project_quickstart'],
       install_requires = install_requires,
-      # If there are data files to include with installation, specify here
-      # (they should be in the main src dir):
-      # Including them in MANIFEST.in is not favoured.
-#      package_data = {'sample': ['package_data.dat']},
-      # 'package_data' is preferred but if data files are outside the package's
-      # main dir then use:
-      # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-      # 'data_file' will be installed into '<sys.prefix>/my_data'
-#      data_files=[('my_data', ['data/data_file'])],
-      package_dir = {CONFIG['metadata']['project_name']: CONFIG['metadata']['project_name']},
-#      entry_points = entry_points,
-       entry_points = {'console_scripts': [
-           'project_quickstart.py = project_quickstart:main'
-           ]},
-      cmdclass = {'install': CustomInstall},
+#      package_dir = {'project_quickstart' : 'project_quickstart'},
+       entry_points = {'console_scripts': [ 'project_quickstart.py = project_quickstart:main' ]},
       zip_safe = False,
-      test_suite = "tests"
           )
 #################
