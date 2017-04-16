ea9d8ee9323ad834cc8c99f00d55e1d034fc09ba
AntonioJBT <a.berlanga@imperial.ac.uk>
2017-04-16 18:08:50 +0100

    ...

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+# Python 2 to 3 changed the use of __init__.py
+# To avoid namespace problems use this for 2 and 3 compatibility
+# https://packaging.python.org/namespace_packages/?highlight=__init__
+# Otherwise omit __init__.py entirely unless you have sub-packages
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
