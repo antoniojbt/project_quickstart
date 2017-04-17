167a3ca51981d3adb98b8edf382dd8bb5173d10d
AntonioJBT <a.berlanga@imperial.ac.uk>
2017-04-17 14:08:21 +0100

    ....

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
       packages = ['project_quickstart'],
       install_requires = install_requires,
 #      package_dir = {'project_quickstart' : 'project_quickstart'},
-      entry_points = {'console_scripts': [ 'project_quickstart.py = project_quickstart:main' ]},
+      entry_points = {'console_scripts': [ 'project_quickstart.py = project_quickstart:project_quickstart' ]},
       zip_safe = False,
           )
 #################
