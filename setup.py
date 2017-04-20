6ededb39fd1a9b421ce7078be94f387e5182d282
AntonioJBT <a.berlanga@imperial.ac.uk>
2017-04-20 12:57:22 +0100

    updates/testing

--- setup.py
+++ setup.py
@@ -1,165 +1,125 @@
-'''
-setup for |project_name|
-
-For example on setting a Python package, see:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/pypa/sampleproject
-
-For Python 3.5
-Before packaging or installing run:
-
-    pip install -U pip twine check-manifest setuptools
-
-TO DO: to add tests see https://python-packaging.readthedocs.io/en/latest/testing.html
-
-To package, do something like this:
-
-    check-manifest
-    python setup.py check
-    python setup.py sdist bdist_wheels
-
-which will create a dist/ directory and a compressed file inside with your package.
-
-More notes and references in:
-    https://github.com/EpiCompBio/welcome
-
-And in the Python docs.
-Upload to PyPI after this if for general use.
-'''
-from __future__ import print_function
-from __future__ import unicode_literals
-from __future__ import division
-from __future__ import absolute_import
-
-# Always prefer setuptools over distutils
-from builtins import open
-from builtins import str
-from future import standard_library
-standard_library.install_aliases()
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-
-# To run custom install warning use:
-from setuptools.command.install import install
-
 import sys
 import os
+import glob
 
-# Get location to this file:
-here = os.path.abspath(os.path.dirname(__file__))
-print(here)
+from ez_setup import use_setuptools
+use_setuptools("10.0")
+import setuptools
 
-# Set up calling parameters from INI file:
-# Modules with Py2 to 3 conflicts
-try:
-    import configparser
-except ImportError:  # Py2 to Py3
-    import Configparser as configparser
+from setuptools import setup, find_packages, Extension
 
-# Global variable for configuration file ('.ini')
-# allow_no_value addition is from:
-# https://github.com/docopt/docopt/blob/master/examples/config_file_example.py
-# By using `allow_no_value=True` we are allowed to
-# write `--force` instead of `--force=true` below.
-CONFIG = configparser.ConfigParser(allow_no_value = True)
+from distutils.version import LooseVersion
+if LooseVersion(setuptools.__version__) < LooseVersion('1.1'):
+    print ("Version detected:", LooseVersion(setuptools.__version__))
+    raise ImportError(
+        "Setuptools 1.1 or higher is required")
 
-CONFIG.read(os.path.join(here, str('project_quickstart' + '.ini')))
-# str(CONFIG['metadata']['project_name'] + '.ini'))) 
-
-# Print keys (sections):
-print('Values for setup.py:', '\n')
-for key in CONFIG:
-    for value in CONFIG[key]:
-        print(key, value, CONFIG[key][value])
-#################
-
-
-#################
-# Get version:
-sys.path.insert(0, CONFIG['metadata']['project_name'])
+########################################################################
+########################################################################
+# collect umi_tools version
+sys.path.insert(0, "project_quickstart")
 import version
 
 version = version.__version__
-print(version)
-#################
 
+###############################################################
+###############################################################
+# Define dependencies 
+# Perform a umi_tools Installation
 
-#################
-# Get info from README and version.py:
-# Get Ptyhon modules required:
-install_requires = []
+major, minor1, minor2, s, tmp = sys.version_info
 
-with open(os.path.join(here, 'requirements.rst'), encoding='utf-8') as required:
-    for line in (required):
-        if not line.startswith('#') and not line.startswith('\n'):
-            line = line.strip()
-            install_requires.append(line)
+if (major == 2 and minor1 < 7) or major < 2:
+    raise SystemExit("""project_quickstart requires Python 2.7 or later.""")
 
-print(install_requires)
+project_quickstart_packages = ["project_quickstart"]
+project_quickstart_package_dirs = {'project_quickstart': 'project_quickstart'}
 
-# Use README as long description if desired, otherwise get it from INI file (or
-# write it out in setup()):
+# debugging pip installation
+#install_requires = []
+#for requirement in (
+#        l.strip() for l in open('requirements.txt') if not l.startswith("#")):
+#    install_requires.append(requirement)
 
-with open(os.path.join(here, 'README.rst'), encoding='utf-8') as readme:
-    description = readme.read()
+install_requires = [
+#    "setuptools>=1.1",
+#    "cython>=0.19",
+#    "numpy>=1.7",
+#    "pandas>=0.12.0",
+    "future",
+    "six",
+    "docopt"]
 
-# Give warning:
-class CustomInstall(install):
-    def initialize_options(self):
-        if sys.version < '3':
-            print('Error during installation: ', '\n',
-                    CONFIG['metadata']['project_name'],
-                    ' requires Python 3x.',
-                    'Exiting...')
-            sys.exit(1)
+# This is a hack. When Pysam is installed from source, the recorded
+# version is 0.2.3, even though a more recent version is actaully
+# installed. In the following, if pysam is not detected, pysam will be
+# install, presumably this will be the lastest version. If pysam is
+# present detect its version with pysam.__version__.  The only problem
+# with this is that if pysam is present, but out of date, the system
+# will not recognise the update
 
-        return install.initialize_options(self)
+#try:
+#    import pysam
+#    if LooseVersion(pysam.__version__) < LooseVersion('0.8.4'):
+#        print("""
+#        
+    ######################################################################
+    #
+    # WARNING:
+    # Pysam is installed, but not recent enough. We will update pysam, but
+    # the system may fail to detect that pysam has been updated. If this
+    # happens please run setup again"
+    #
+    ######################################################################
+#        """)
 
-# See
-# http://setuptools.readthedocs.io/en/latest/setuptools.html#dynamic-discovery-of-services-and-plugins
-# For rst INI entry points
-#################
+#        install_requires.append("pysam>=0.8.4")
 
+#except ImportError:
+#    install_requires.append("pysam")
 
-#################
-# Actual setup.py instructions
-# Python docs: https://docs.python.org/3.6/distutils/setupscript.html 
-# Tutorial: http://python-packaging.readthedocs.io/en/latest/
+##########################################################
+##########################################################
+# Classifiers
+classifiers = """
+Development Status :: 3 - Alpha
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+License :: OSI Approved
+Programming Language :: Python
+Topic :: Software Development
+Topic :: Scientific/Engineering
+Operating System :: POSIX
+Operating System :: Unix
+Operating System :: MacOS
+"""
+
 setup(
-      # Package metadata:
-      name = CONFIG['metadata']['project_name'],
-      version = CONFIG['metadata']['version'],
-      url = CONFIG['metadata']['project_url'],
-      download_url = CONFIG['metadata']['download_url'],
-      author = CONFIG['metadata']['author_name'],
-      author_email = CONFIG['metadata']['author_email'],
-      license = CONFIG['metadata']['license'],
-#      classifiers = [CONFIG['metadata']['classifiers_setup']], 
-       # needs to be passed as list
-       # gives many errors when registering manually in pip
-      description = CONFIG['metadata']['project_short_description'],
-      keywords = CONFIG['metadata']['keywords'],
-      #long_description = CONFIG['metadata']['long_description'],
-      long_description = description,
-      # Package information:
-      packages = find_packages(),
-      install_requires = install_requires,
-      include_package_data = True,
-      # If there are data files to include with installation, specify here
-      # (they should be in the main src dir):
-      # Including them in MANIFEST.in is not favoured.
-#      package_data = {'sample': ['package_data.dat']},
-      # 'package_data' is preferred but if data files are outside the package's
-      # main dir then use:
-      # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-      # 'data_file' will be installed into '<sys.prefix>/my_data'
-#      data_files=[('templates', ['templates/*', 'templates/*/*'])],
-      #package_dir = {CONFIG['metadata']['project_name']: CONFIG['metadata']['project_name']},
-      scripts = [str(CONFIG['metadata']['project_name'] + '/main.py')],
-      #entry_points = {'console_scripts': ['project_quickstart.py = project_quickstart.project_quickstart:main']},
-      cmdclass = {'install': CustomInstall},
-      zip_safe = False,
-      #test_suite = "tests"
-          )
-#################
+    # package information
+    name='project_quickstart',
+    version=version,
+    description='Boilerplate tools to quickly create a data science project skeleton',
+    author='Antonio J Berlanga Taylor',
+    author_email='i.sudbery@sheffield.ac.uk',
+    license="GPL-3",
+    platforms=["any"],
+    keywords="computational genomics",
+    long_description='project_quickstart',
+    classifiers=list(filter(None, classifiers.split("\n"))),
+    url="https://github.com/AntonioJBT/project_quickstart",
+    download_url="https://github.com/AntonioJBT/project_quickstart",
+    # package contents
+    packages=project_quickstart_packages,
+    package_dir=project_quickstart_package_dirs,
+    include_package_data=True,
+    # dependencies
+    #setup_requires=['cython'],
+    install_requires=install_requires,
+    # extension modules
+    # ext_modules=cythonize("umi_tools/_dedup_umi.pyx"),
+    entry_points={
+        'console_scripts': ['project_quickstart = project_quickstart.project_quickstart:main']
+    },
+    # other options
+    zip_safe=False,
+)
