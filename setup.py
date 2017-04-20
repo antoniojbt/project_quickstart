9b5b38e0e31d49877ca3c6e0d03e08f396e25610
AntonioJBT <a.berlanga@imperial.ac.uk>
2017-04-20 13:22:07 +0100

    updates/testing

--- setup.py
+++ setup.py
@@ -1,13 +1,60 @@
+'''
+setup for |project_name|
+
+For example on setting a Python package, see:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+
+For Python 3.5
+Before packaging or installing run:
+
+    pip install -U pip twine check-manifest setuptools
+
+TO DO: to add tests see https://python-packaging.readthedocs.io/en/latest/testing.html
+
+To package, do something like this:
+
+    check-manifest
+    python setup.py check
+    python setup.py sdist bdist_wheels
+
+which will create a dist/ directory and a compressed file inside with your package.
+
+More notes and references in:
+    https://github.com/EpiCompBio/welcome
+
+And in the Python docs.
+Upload to PyPI after this if for general use.
+'''
+#################
+# Get modules
+
+# Py3 to 2 from pasteurize:
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+from __future__ import absolute_import
+
+from builtins import open
+from builtins import str
+from future import standard_library
+standard_library.install_aliases()
+
+# To use a consistent encoding
+from codecs import open
+
+# Standard modules:
 import sys
 import os
 import glob
 
-#from ez_setup import use_setuptools
-#use_setuptools("10.0")
-
+# Always prefer setuptools over distutils:
 import setuptools
 
-from setuptools import setup, find_packages, Extension
+from setuptools import setup, find_packages
+
+# To run custom install warning use:
+from setuptools.command.install import install
 
 from distutils.version import LooseVersion
 if LooseVersion(setuptools.__version__) < LooseVersion('1.1'):
@@ -15,73 +62,69 @@ if LooseVersion(setuptools.__version__) < LooseVersion('1.1'):
     raise ImportError(
         "Setuptools 1.1 or higher is required")
 
-########################################################################
-########################################################################
-# collect umi_tools version
-sys.path.insert(0, "project_quickstart")
+# Get location to this file:
+here = os.path.abspath(os.path.dirname(__file__))
+print(here)
+
+# Set up calling parameters from INI file:
+# Modules with Py2 to 3 conflicts
+try:
+    import configparser
+except ImportError:  # Py2 to Py3
+    import Configparser as configparser
+
+# Global variable for configuration file ('.ini')
+# allow_no_value addition is from:
+# https://github.com/docopt/docopt/blob/master/examples/config_file_example.py
+# By using `allow_no_value=True` we are allowed to
+# write `--force` instead of `--force=true` below.
+CONFIG = configparser.ConfigParser(allow_no_value = True)
+
+CONFIG.read(os.path.join(here, str('project_quickstart' + '.ini')))
+# str(CONFIG['metadata']['project_name'] + '.ini'))) 
+
+# Print keys (sections):
+print('Values for setup.py:', '\n')
+for key in CONFIG:
+    for value in CONFIG[key]:
+        print(key, value, CONFIG[key][value])
+#################
+
+
+#################
+# Get version:
+sys.path.insert(0, CONFIG['metadata']['project_name'])
 import version
 
 version = version.__version__
+#################
 
-###############################################################
-###############################################################
-# Define dependencies 
-# Perform a umi_tools Installation
 
+#################
+# Define dependencies for installation
+
+# Python version needed:
 major, minor1, minor2, s, tmp = sys.version_info
 
 if (major == 2 and minor1 < 7) or major < 2:
     raise SystemExit("""project_quickstart requires Python 2.7 or later.""")
 
+# Dependencies for installation process:
+install_requires = [
+        "future",
+        "six",
+        "docopt"]
+#################
+
+
+
+#################
+# Define project specific elements:
 project_quickstart_packages = ["project_quickstart"]
 project_quickstart_package_dirs = {'project_quickstart': 'project_quickstart'}
 
-# debugging pip installation
-#install_requires = []
-#for requirement in (
-#        l.strip() for l in open('requirements.txt') if not l.startswith("#")):
-#    install_requires.append(requirement)
 
-install_requires = [
-#    "setuptools>=1.1",
-#    "cython>=0.19",
-#    "numpy>=1.7",
-#    "pandas>=0.12.0",
-    "future",
-    "six",
-    "docopt"]
-
-# This is a hack. When Pysam is installed from source, the recorded
-# version is 0.2.3, even though a more recent version is actaully
-# installed. In the following, if pysam is not detected, pysam will be
-# install, presumably this will be the lastest version. If pysam is
-# present detect its version with pysam.__version__.  The only problem
-# with this is that if pysam is present, but out of date, the system
-# will not recognise the update
-
-#try:
-#    import pysam
-#    if LooseVersion(pysam.__version__) < LooseVersion('0.8.4'):
-#        print("""
-#        
-    ######################################################################
-    #
-    # WARNING:
-    # Pysam is installed, but not recent enough. We will update pysam, but
-    # the system may fail to detect that pysam has been updated. If this
-    # happens please run setup again"
-    #
-    ######################################################################
-#        """)
-
-#        install_requires.append("pysam>=0.8.4")
-
-#except ImportError:
-#    install_requires.append("pysam")
-
-##########################################################
-##########################################################
-# Classifiers
+# Classifiers:
 classifiers = """
 Development Status :: 3 - Alpha
 Intended Audience :: Science/Research
@@ -94,33 +137,36 @@ Operating System :: POSIX
 Operating System :: Unix
 Operating System :: MacOS
 """
+#################
+
+
+#################
+# Run actual setup:
 
 setup(
-    # package information
-    name='project_quickstart',
-    version=version,
-    description='Boilerplate tools to quickly create a data science project skeleton',
-    author='Antonio J Berlanga Taylor',
-    author_email='i.sudbery@sheffield.ac.uk',
-    license="GPL-3",
-    platforms=["any"],
-    keywords="computational genomics",
-    long_description='project_quickstart',
-    classifiers=list(filter(None, classifiers.split("\n"))),
-    url="https://github.com/AntonioJBT/project_quickstart",
-    download_url="https://github.com/AntonioJBT/project_quickstart",
-    # package contents
-    packages=project_quickstart_packages,
-    package_dir=project_quickstart_package_dirs,
-    include_package_data=True,
-    # dependencies
-    #setup_requires=['cython'],
-    install_requires=install_requires,
-    # extension modules
-    # ext_modules=cythonize("umi_tools/_dedup_umi.pyx"),
+        # Package information:
+    name = 'project_quickstart',
+    version = version,
+    description = 'Boilerplate tools to quickly create a data science project skeleton',
+    author = 'Antonio J Berlanga Taylor',
+    author_email = 'a.berlanga@imperial.ac.uk',
+    license = "GPL-3",
+    platforms = ["any"],
+    keywords = "computational genomics",
+    long_description = 'project_quickstart',
+    classifiers = list(filter(None, classifiers.split("\n"))),
+    url = "https://github.com/AntonioJBT/project_quickstart",
+    download_url = "https://github.com/AntonioJBT/project_quickstart",
+    # Package contents:
+    packages = project_quickstart_packages,
+    package_dir = project_quickstart_package_dirs,
+    include_package_data = True,
+    # Dependencies:
+    install_requires = install_requires,
     entry_points={
         'console_scripts': ['project_quickstart = project_quickstart.project_quickstart:main']
     },
-    # other options
-    zip_safe=False,
-)
+    # Other options:
+    zip_safe = False,
+    )
+#################
