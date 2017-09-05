27036e76340ef123c4215072c001baccacae42ff
AntonioJBT <a.berlanga@imperial.ac.uk>
2017-09-05 22:09:11 +0100

    runnable svgutils example in template scripts for multi-panel figures

--- templates/script_templates/svgutils_template.py
+++ templates/script_templates/svgutils_template.py
@@ -2,73 +2,134 @@
 # Examples from svgutils
 # https://neuroscience.telenczuk.pl/?p=331
 # http://svgutils.readthedocs.io/en/latest/tutorials/composing_multipanel_figures.html
+# https://svgutils.readthedocs.io/en/latest/compose.html
 # http://cairosvg.org/
 # Use inkscape instead of cairo?
 ##############
 
 
-##############
-#####
-import svgutils as su # compose, transform, templates
+############
+# First generate some example data to make this script runnable
+# This is modified from 
+# http://users.ecs.soton.ac.uk/jn2/teaching/pythonLecture.html
+
+import matplotlib.pyplot as plt
+import random
+
+# Make a first example plot:
+popSize = 100
+category1 = []
+category2 = []
+
+# Start by generating scores for category 1 individuals
+# Their mean score is 95
+for i in range(popSize):
+    category1.append(random.normalvariate(95,10))
+
+# Now generate scores for category 2 individuals
+# They have a higher mean of 105
+for i in range(popSize):
+    category2.append(random.normalvariate(105,10))
+
+scores = [category1,category2]
+
+plt.boxplot(scores)
+plt.savefig('boxplots.svg')
+plt.savefig('boxplots.pdf')
+plt.close()
+
+# Make a second example plot:
+x = []
+y = []
+
+sampleSize = 500
+
+for i in range(sampleSize):
+    newVal = random.normalvariate(100,10)
+    x.append(newVal)
+    y.append(newVal / 2.0 + random.normalvariate(50,5))
+
+plt.scatter(x,y,c="red",marker="s")
+plt.xlabel("Variable 1")
+plt.ylabel("Variable 2")
+plt.savefig("scatter2.svg")
+plt.savefig('scatter2.pdf')
+plt.close()
+############
+
+
+############
+# Make the actual panel with svgutils
+from svgutils.compose import *
 import cairosvg
-import sys
 import os
-#####
 
-#####
-#create new SVG figure:
-fig = su.transform.SVGFigure("16cm", "6.5cm")
+# Names of the plots to read in, these have to be svg files:
+plot1 = 'boxplots.svg' # These are taken from above as example data
+plot2 = 'scatter2.svg'
 
-# load svg figures (preferably matpotlib-generated):
-fig1 = su.transform.fromfile('sigmoid_fit.svg')
-fig2 = su.transform.fromfile('anscombe.svg')
-# Test also simply:
-#imported_svg1 = '/path_to/XXXX.svg'
-#imported_svg2 = '/path_to/XXXX.svg'
+# Name the figure panel (which will be "F{figure_number}_{figure_name}.{format}", 
+# Only svg can be output with svgutils:
+figure_number = '1'
+figure_name = 'test'
+file_format_in = 'svg'
+file_format_out = 'pdf'
+layout_name_1 = str('F{}_{}.{}'.format(figure_number,
+                                       figure_name,
+                                       file_format_in))
+layout_name_2 = str('F{}_{}.{}'.format(figure_number,
+                                       figure_name,
+                                       file_format_out))
+
+my_layout = Figure("21cm", "19cm", # A4 paper is 210 mm x 197 mm
+                  # Panel() groups all elements belonging to one plot/panel
+                  Panel(
+                      SVG(plot1).scale(0.8), # scale only the plot, not the text
+                      # Place Text() after SVG(), otherwise it doesn't plot:
+                      Text("A", 25, 20, size = 11, weight = 'bold'),
+                      ).move(0, 10),
+                  Panel(
+                      SVG(plot2).scale(0.8),
+                      Text("B", 25, 20, size = 11, weight = 'bold'),
+                      ).move(340, 10), # placed here move() is applied to all
+                                       # elements of the panel
+                                       # move(280, 0) will move the figure 280
+                                       # px horizontally
+                  #Grid(20, 20) # Generates a grid of horizontal and vertical lines 
+                               # labelled with their position in pixel units
+                               # Use to test if figure is placed correctly, then
+                               # comment out. Use within Figure()
+                               # e.g. Figure(XXX, Grid(20, 20), Panel(XXX))
+                    )
+                     #.tile(2, 1) # (ncols, nrows), use before .save()
+                     #.tile() errors, see:
+                     #https://stackoverflow.com/questions/45850144/is-there-a-bug-in-svgutils-compose-module-regarding-the-arrangement-of-figures/45863869#45863869
+
+# Save the Figure:
+my_layout.save(layout_name_1)
+
+# Convert SVG file to PDF with CairoSVG:
+cairosvg.svg2pdf(url = layout_name_1,
+                 write_to = layout_name_2
+                 )
+
+# Alternatively with inkscape:
+#os.system('''inkscape --export-pdf=F{}_{}.ink.{} {}'''.format(figure_number,
+#                                                              figure_name,
+#                                                              file_format_out,
+#                                                              layout_name_1)
+#          )
+
+# Inkscape is very power and has many more options, e.g. 
+# --export-background=white --export-area-drawing
+# See:
+#https://inkscape.org/sk/doc/inkscape-man.html
 
 
-# get the plot objects:
-plot1 = fig1.getroot()
-plot2 = fig2.getroot()
-plot2.moveto(280, 0, scale=0.5)
+# TO DO: How to add legends directly?
+# Otherwise pull into an rst file and add there. 
+############
 
-# add text labels:
-txt1 = su.transform.TextElement(25,20, "A", size=12, weight="bold")
-txt2 = su.transform.TextElement(305,20, "B", size=12, weight="bold")
-
-# append plots and labels to figure
-fig.append([plot1, plot2])
-fig.append([txt1, txt2])
-
-# save generated SVG files
-fig.save("fig_final.svg")
-
-# Convert SVG file to PDF:
-cairosvg.svg2pdf(url='fig_final_compose.svg', write_to='fig_final_compose.pdf')
-#inkscape --export-pdf=fig_final.pdf fig_final.svg
-#####
-
-#####
-#CONFIG['figure.save_path'] = 'composing_multipanel_figures'
-
-Figure("16cm", "6.5cm",
-       Panel(
-             Text("A", 25, 20, size=12, weight='bold'),
-             SVG("sigmoid_fit.svg")
-            ),
-       Panel(
-             Text("B", 25, 20).move(280, 0),
-             SVG("anscombe.svg").scale(0.5)
-             ).move(280, 0),
-#             Grid(20, 20)
-       ).tile(2, 1)
-        .save("F{}_{}.svg".format(figure_number, figure_legend))
-# Convert SVG file to PDF:
-cairosvg.svg2pdf(url='fig_final_compose.svg', write_to='fig_final_compose.pdf')
-#inkscape --export-pdf=fig_final.pdf fig_final.svg
-#####
-##############
-
-##############
+############
 quit()
 ##############
