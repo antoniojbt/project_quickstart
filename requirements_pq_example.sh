04a03b50d2757f0e010853c5c28a424df7d97074
AntonioJBT <a.berlanga@imperial.ac.uk>
2018-06-19 15:50:36 +0100

    updated requirements bash script

--- requirements_pq_example.sh
+++ requirements_pq_example.sh
@@ -13,28 +13,33 @@ set -o nounset
 # cgat
 # dependencies for project_quickstart's example
 
-conda create -yn py35_test python=3.5
+# Create conda environment:
+bash -c 'conda create -yn pq_test python'
+bash -c 'conda activate pq_test'
 
-bash -c 'source activate py35_test ; \
-         wget https://raw.githubusercontent.com/AntonioJBT/project_quickstart/master/requirements.rst ; \
+# Install project_quickstart and pq_example requirements:
+bash -c 'wget https://raw.githubusercontent.com/AntonioJBT/project_quickstart/master/requirements.rst ; \
          pip install -r requirements.rst ; \
          pip install svgutils cairosvg ; \
          pip install --upgrade git+git://github.com/AntonioJBT/project_quickstart.git ; \
-         wget https://raw.githubusercontent.com/AntonioJBT/project_quickstart/master/requirements.rst ; \
-         pip install -r requirements.rst ; \
-         pip install --upgrade git+git://github.com/AntonioJBT/project_quickstart.git ; \
-         wget https://raw.githubusercontent.com/AntonioJBT/CGATPipeline_core/master/requirements.txt ; \
-         pip install -r requirements.txt ; \
-         pip install git+git://github.com/AntonioJBT/CGATPipeline_core.git ; \
-         pip install cgat ; \
          pip install sphinxcontrib-bibtex ; \
-         conda install -y rpy2 ; \
          conda install -y r-docopt ; \
-         conda install -y r-data.table ; \
-         R --vanilla -e 'source("https://bioconductor.org/biocLite.R") ; install.packages("stargazer", repos = "http://cran.us.r-project.org") ; library("stargazer")' ; \
+         conda install -y r-data.table'
+
+# Get R packages not available with conda:
+bash -c 'R --vanilla -e 'source("https://bioconductor.org/biocLite.R") ; install.packages("stargazer", repos = "http://cran.us.r-project.org") ; library("stargazer")' ; \
          conda install -y r-ggplot2 ; \
          R --vanilla -e 'source("https://bioconductor.org/biocLite.R") ; install.packages("svglite", repos = "http://cran.us.r-project.org") ; library("svglite")' ; \
-         conda install -y r-string ; \
-         source deactivate'
+         conda install -y r-string'
 
-echo 'Activate the environment in your terminal running "source activate py35_test"'
+# Install CGAT tools with fork:
+#bash -c 'wget https://raw.githubusercontent.com/AntonioJBT/CGATPipeline_core/master/requirements.txt ; \
+#         pip install -r requirements.txt ; \
+#         pip install git+git://github.com/AntonioJBT/CGATPipeline_core.git ; \
+#         pip install cgat ; \
+#         conda install -y rpy2'
+
+# Finish and deactivate conda environment:
+bash -c 'conda deactivate'
+echo "All done, packages downloaded and installed. Activate your environment
+using 'conda activate pq_test'"
