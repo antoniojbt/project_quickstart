cb175c0fceffe8a3255fd1f3dbf9dd34d31cca0f
AntonioJBT <a.berlanga@imperial.ac.uk>
2019-01-25 17:03:56 +0000

    updating test files

--- tests/ref_files/pq_example/code/pq_example/examples.sh
+++ tests/ref_files/pq_example/code/pq_example/examples.sh
@@ -77,8 +77,8 @@ ${python_exec} ../code/pq_example/svgutils_pq_example.py --plotA=${plotA} \
 
 ###########################
 # Create the report:
-cp -r ../code/pq_example/pipeline_pq_example/configuration .
-cd configuration
+cp -r ../code/pq_example/pipeline_pq_example/configuration_pipeline_pq_example .
+cd configuration_pipeline_pq_example
 make html
 ln -fs _build/html/report_pipeline_pq_example.html .
 make latexpdf
